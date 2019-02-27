530cc1423a6acac0670513eca0a05b8c9c338d9e
Charles Sperbeck <csperbeck@hchb.com>
2019-02-27 18:57:22 -0500

    added build.cmd

--- build.cmd
+++ build.cmd
@@ -1,11 +1,11 @@
 call npm ls --global --depth=0
-if %ERRORLEVEL% NEQ 0 (
-    echo "Please download and install NPM from https://nodejs.org/en/"
-) else (
-    call npm run preinstall && npm run clean:build
-    if NOT %ERRORLEVEL% NEQ 0 (
-        echo The Azure-Bake Project has built successfully with LERNA.
-    ) else ( 
-        echo The Azure-Bake Project failed to build. Please view console and logging errors for additional information.
+if %ERRORLEVEL% == 0 (    
+    call npm ls lerna --global --depth=0 | findstr /i lerna
+    if %ERRORLEVEL% == 1 (
+        call npm run clean:build
+        if %ERRORLEVEL% == 0 echo The Azure-Bake Project has built successfully with LERNA
+    ) else (
+        call npm run preinstall && npm run clean:build 
+        if %ERRORLEVEL% == 0 echo The Azure-Bake Project has installed necessary tools and built successfully with LERNA.
     )
-)
+) else ( echo "Please download and install NPM from https://nodejs.org/en/" )
