5fa3d3c2e9f4dd1991c41f74807bbf2ab08e133c
Charles Sperbeck <csperbeck@hchb.com>
2019-03-05 14:04:38 -0500

    added test scripts

---
+++ ingredient/ingredient-storage/src/index.ts
@@ -0,0 +1,5 @@
+import {StoragePlugIn} from './plugin'
+
+/*  comment out these entries if you are not including an ingredient plugin runner*/
+exports.plugin = StoragePlugIn
+exports.pluginNS = "@azbake/ingredient-storage" //name of the ingredient to reference in a bake.yaml recipe
