ba062b560b5522ef11f6fcc3341e69b2cf6e8989
Charles Sperbeck <csperbeck@hchb.com>
2019-03-06 16:20:58 -0500

    update storage

--- ingredient/ingredient-storage/src/index.ts
+++ ingredient/ingredient-storage/src/index.ts
@@ -1,5 +1,4 @@
 import {StoragePlugIn} from './plugin'
 
-/*  comment out these entries if you are not including an ingredient plugin runner*/
 exports.plugin = StoragePlugIn
 exports.pluginNS = "@azbake/ingredient-storage" //name of the ingredient to reference in a bake.yaml recipe
