d8dc7c8f8ca3774665f5a5464538914b14b7dc73
Charles Sperbeck <csperbeck@hchb.com>
2019-03-05 20:09:04 -0500

    working test files

--- ingredient/ingredient-storage/test/entrypoint.sh
+++ ingredient/ingredient-storage/test/entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-cd /usr/local/azure-bake
-if npm run clean:build
-then 
-    cd /ingredient/ingredient-storage/test
-    if [[ -z "${container_uri}" ]]
-        then $deployment_name = "storage-test:dev"
-        else $deployment_name = "$container_uri/storage-test:dev"; fi
-    fi
-    node /usr/local/azure-bake/system/dist/index.js mix --name "$container_uri/storage-test:dev" --runtime "latest" test.yaml
-    node /usr/local/azure-bake/system/dist/index.js serve "$container_uri/storage-test:dev"
-else
-    echo "NPM Clean:Build FAILED! Exiting...."
-fi
+cd /usr/local
+git clone --single-branch --branch "${git_branch}" "${git_repo}"
+cd azure-bake
+npm run clean:build
