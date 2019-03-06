e3741157d8ff81f67403364d551083ff9c098358
Charles Sperbeck <csperbeck@hchb.com>
2019-03-05 20:09:04 -0500

    working test files

--- ingredient/ingredient-storage/test/run.ps1
+++ ingredient/ingredient-storage/test/run.ps1
@@ -25,30 +25,28 @@ else { Write-Host "Azure Service Principal Key is already set!" }
 
 if ([string]::IsNullOrEmpty($ENV:CONTAINER_URI)) 
 {
-    [Environment]::SetEnvironmentVariable('BAKE_AUTH_SERVICE_KEY', $(Read-Host "Enter container repostiory URL"))
+    [Environment]::SetEnvironmentVariable('CONTAINER_URI', $(Read-Host "Enter container repostiory URL"))
 }
 else { Write-Host "Container Repository URL is already set!" }
 
-#Run different dockerfile depending on run location
-if ([string]::IsNullOrEmpty($ENV:AGENT_ID))
-{
-    #Set variables for local repo
-    $git_repo = $(git config --get remote.origin.url)
-    $git_branch = $(git rev-parse --abbrev-ref HEAD)
-}
-else
-{
-    #Set environment variables as local for Dockerfile
-    $git_repo = "$ENV:BUILD_REPOSITORY_URI"
-    $git_branch = "$ENV:BUILD_SOURCEBRANCH"
-}
+$global:git_repo = $(git config --get remote.origin.url)
+$global:git_branch = $(git rev-parse --abbrev-ref HEAD)
+
+Get-Content ./template.yml -Raw | Foreach-Object {
+    $_  -replace '\$\{git_repo\}', $git_repo `
+    -replace '\$\{git_branch\}', $git_branch `
+    -replace '\$\{BAKE_AUTH_SUBSCRIPTION_ID\}', $ENV:BAKE_AUTH_SUBSCRIPTION_ID `
+    -replace '\$\{BAKE_AUTH_SERVICE_ID\}', $ENV:BAKE_AUTH_SERVICE_ID `
+    -replace '\$\{BAKE_AUTH_SERVICE_KEY\}', $ENV:BAKE_AUTH_SERVICE_KEY `
+    -replace '\$\{BAKE_AUTH_TENANT_ID\}', $ENV:BAKE_AUTH_TENANT_ID `
+    -replace '\$\{CONTAINER_URI\}', $ENV:CONTAINER_URI `
+} | Set-Content ./docker-compose.yml 
+
+docker-compose up --remove-orphans --build --force-recreate
+Remove-Item ./docker-compose.yml
 
 #Run and push the container
-docker-compose up
 if (![string]::IsNullOrEmpty($ENV:CONTAINER_URI)) 
 { 
-    docker push "$ENV:CONTAINER_URI/storage-test" 
+    docker push "$ENV:CONTAINER_URI/storage-test:latest" 
 }
-
-#Run the container with environment secrets
-docker run -e "container_uri=$ENV:CONTAINER_URI"-e "BAKE_AUTH_SUBSCRIPTION_ID=$ENV:BAKE_AUTH_SUBSCRIPTION_ID" -e "BAKE_AUTH_SERVICE_ID=$ENV:BAKE_AUTH_SERVICE_ID" -e "BAKE_AUTH_SERVICE_KEY=$ENV:BAKE_AUTH_SERVICE_KEY" -e "BAKE_AUTH_TENANT_ID=$ENV:BAKE_AUTH_TENANT" -rm storage-test:latest
