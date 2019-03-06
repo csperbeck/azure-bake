98879133258300630200d43156d411d80d330007
Charles Sperbeck <csperbeck@hchb.com>
2019-03-06 16:20:58 -0500

    update storage

--- ingredient/ingredient-storage/test/run.ps1
+++ ingredient/ingredient-storage/test/run.ps1
@@ -2,30 +2,35 @@
 if ([string]::IsNullOrEmpty($ENV:BAKE_AUTH_SUBSCRIPTION_ID)) 
 {
     [Environment]::SetEnvironmentVariable('BAKE_AUTH_SUBSCRIPTION_ID', $(Read-Host "Enter the Azure Subscription ID"))
+    $global:BAKE_AUTH_SUBSCRIPTION_ID = $ENV:BAKE_AUTH_SUBSCRIPTION_ID
 }
 else { Write-Host "Azure Subscription ID is already set!" }
 
 if ([string]::IsNullOrEmpty($ENV:BAKE_AUTH_TENANT_ID)) 
 {
     [Environment]::SetEnvironmentVariable('BAKE_AUTH_TENANT_ID', $(Read-Host "Enter the Azure Tenant ID"))
+    $global:BAKE_AUTH_TENANT_ID = $ENV:BAKE_AUTH_TENANT_ID
 }
 else { Write-Host "Azure Tenant ID is already set!" }
 
 if ([string]::IsNullOrEmpty($ENV:BAKE_AUTH_SERVICE_ID)) 
 {
     [Environment]::SetEnvironmentVariable('BAKE_AUTH_SERVICE_ID', $(Read-Host "Enter the Azure Service Principal ID"))
+    $global:BAKE_AUTH_SERVICE_ID = $ENV:BAKE_AUTH_SERVICE_ID
 }
 else { Write-Host "Azure Service Principal ID is already set!" }
 
 if ([string]::IsNullOrEmpty($ENV:BAKE_AUTH_SERVICE_KEY)) 
 {
     [Environment]::SetEnvironmentVariable('BAKE_AUTH_SERVICE_KEY', $(Read-Host "Enter the Azure Service Principal Key"))
+    $global:BAKE_AUTH_SERVICE_KEY = $ENV:BAKE_AUTH_SERVICE_KEY
 }
 else { Write-Host "Azure Service Principal Key is already set!" }
 
 if ([string]::IsNullOrEmpty($ENV:CONTAINER_URI)) 
 {
     [Environment]::SetEnvironmentVariable('CONTAINER_URI', $(Read-Host "Enter container repostiory URL"))
+    $global:CONTAINER_URI = $ENV:CONTAINER_URI
 }
 else { Write-Host "Container Repository URL is already set!" }
 
@@ -42,11 +47,12 @@ Get-Content ./template.yml -Raw | Foreach-Object {
     -replace '\$\{CONTAINER_URI\}', $ENV:CONTAINER_URI `
 } | Set-Content ./docker-compose.yml 
 
-docker-compose up --remove-orphans --build --force-recreate
-Remove-Item ./docker-compose.yml
+#docker-compose up --remove-orphans --build --force-recreate
+#Remove-Item ./docker-compose.yml
 
 #Run and push the container
 if (![string]::IsNullOrEmpty($ENV:CONTAINER_URI)) 
 { 
+    docker tag storage-test 'charlietest.azurecr.io/storage-test'
     docker push "$ENV:CONTAINER_URI/storage-test:latest" 
 }
