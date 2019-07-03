d0dbcbc3d27d256c144aaf6033ee23b75002a828
Charlie Sperbeck <csperbeck@hchb.com>
2019-07-03 18:33:55 -0400

    base ingredients

---
+++ ingredient/ingredient-api-managment-api/src/index.ts
@@ -0,0 +1,10 @@
+import { ApimApi } from './plugin'
+import { ApimApiUtils } from './functions'
+
+/*  comment out these entries if you are not including an ingredient plugin runner*/
+exports.plugin = ApimApi
+exports.pluginNS = "@azbake/ingredient-api-management-api" //name of the ingredient to reference in a bake.yaml recipe
+
+/* comment out these entries if you are not including a set of expression functions*/
+exports.functions = ApimApiUtils
+exports.functionsNS = "apim-api" //bake.yaml expressions can access your functions via "myutils.my_function()"
---
+++ ingredient/ingredient-api-managment-base/src/plugin.ts
@@ -0,0 +1,18 @@
+import { BaseIngredient, IngredientManager } from "@azbake/core"
+import { ARMHelper } from "@azbake/arm-helper"
+import ApimTemplate from "./api-management.json"
+export class ApimBase extends BaseIngredient {
+
+    public async Execute(): Promise<void> {
+        try {
+            let util = IngredientManager.getIngredientFunction("coreutils", this._ctx)            
+            this._logger.log(`Azure API Manamgement - Base Logging: ${this._ingredient.properties.source}`)
+            const helper = new ARMHelper(this._ctx);
+            let params = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
+            await helper.DeployTemplate(this._name, WinTemplate, params, await util.resource_group())             
+        } catch(error){
+            this._logger.error('deployment failed: ' + error)
+            throw error
+        }
+    }
+}
