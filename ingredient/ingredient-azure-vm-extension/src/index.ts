403688ccd4e912162fd1cb2c407170190f624695
Kevin Reilly <kevinreilly@hchb.com>
2019-07-22 09:42:03 -0400

    initial vm extension commit

---
+++ ingredient/ingredient-azure-vm-extension/src/index.ts
@@ -0,0 +1,10 @@
+import {AzureVMExtension} from './plugin'
+import {VMExtUtils} from './functions'
+
+/*  comment out these entries if you are not including an ingredient plugin runner*/
+exports.plugin = AzureVMExtension
+exports.pluginNS = "@azbake/ingredient-azure-vm-extension" //name of the ingredient to reference in a bake.yaml recipe
+
+/* comment out these entries if you are not including a set of expression functions*/
+exports.functions = VMExtUtils
+exports.functionsNS = "myutils" //bake.yaml expressions can access your functions via "myutils.my_function()"
