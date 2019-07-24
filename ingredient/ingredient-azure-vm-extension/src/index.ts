cadcf09108f5f899f27e3d7dbfd99b06b7c03c20
Kevin Reilly <kevinreilly@hchb.com>
2019-07-24 14:53:31 -0400

    added framework logic to construct portions of the ARM template based on recipe parameters

--- ingredient/ingredient-azure-vm-extension/src/index.ts
+++ ingredient/ingredient-azure-vm-extension/src/index.ts
@@ -3,5 +3,12 @@ import {VirtualMachineExtensionsUtils} from './functions'
 
 exports.plugin = VirtualMachineExtensions
 exports.pluginNS = "@azbake/ingredient-azure-vm-extension" //name of the ingredient to reference in a bake.yaml recipe
+<<<<<<< HEAD
 exports.functions = VirtualMachineExtensionsUtils
 exports.functionsNS = "vmextensionsutility" //bake.yaml expressions can access your functions via "myutils.my_function()"
+=======
+
+/* comment out these entries if you are not including a set of expression functions*/
+exports.functions = VMExtUtils
+exports.functionsNS = "vmextutils" //bake.yaml expressions can access your functions via "myutils.my_function()"
+>>>>>>> added framework logic to construct portions of the ARM template based on recipe parameters
