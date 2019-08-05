7229daa7d38c48c10904b7b4efd97a9eb9279fb5
Charlie Sperbeck <csperbeck@hchb.com>
2019-08-05 13:51:18 -0400

    added testing recipe

--- ingredient/ingredient-azure-kubernetes-service/src/plugin.ts
+++ ingredient/ingredient-azure-kubernetes-service/src/plugin.ts
@@ -10,7 +10,11 @@ export class AzureKubernetesService extends BaseIngredient {
             this._logger.log(`Azure Container Service Plugin Logging: ${this._ingredient.properties.source}`)
             const helper = new ARMHelper(this._ctx);
             let params: any = await helper.BakeParamsToARMParamsAsync(this._name, this._ingredient.properties.parameters)
-            let containerClient: ContainerServiceClient = new ContainerServiceClient(this._ctx.AuthToken,this._ctx.Environment.authentication.subscriptionId)                                        
+            let containerClient: ContainerServiceClient = new ContainerServiceClient(this._ctx.AuthToken,this._ctx.Environment.authentication.subscriptionId)
+            this._logger.log(`Name: ${util.name}`)
+            this._logger.log(`Keys: ${Object.keys(util)}`)
+
+            //containerClient.containerServices.beginCreateOrUpdate(await util.resource_group)
         } catch(error){
             this._logger.error('deployment failed: ' + error)
             throw error
